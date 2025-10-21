385c0cc29488381df6b92791969245eaa9c35dab
Andre Beolchi <andregarbini2001@gmail.com>
2025-10-21 18:37:39 -0300

    feat(controllers): implement get cart controller
    closes #23, closes #6

---
+++ src/adapters/controllers/cart/get-cart-controller.test.ts
@@ -0,0 +1,38 @@
+import { mock } from 'jest-mock-extended'
+
+import { VerifyTokenUseCase } from '~/domain/auth/application/use-cases/verify-token-use-case'
+import { GetCartUseCase } from '~/domain/cart/application/use-cases/get-cart-use-case'
+
+import { cartFactory, userFactory } from '~/infra/fixtures'
+import { Logger } from '~/infra/logger'
+
+import { GetCartController } from './get-cart-controller'
+
+describe('[controller] get cart', () => {
+  const logger = mock<Logger>()
+  const verifyTokenUseCase = mock<VerifyTokenUseCase>()
+  const getCartUseCase = mock<GetCartUseCase>()
+
+  const getCartController = new GetCartController(logger, verifyTokenUseCase, getCartUseCase)
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should return 200 and cart data on successful retrieval', async () => {
+    const user = userFactory.build()
+    const cartData = cartFactory.build()
+
+    verifyTokenUseCase.execute.mockResolvedValueOnce(user)
+    getCartUseCase.execute.mockResolvedValueOnce(cartData)
+
+    const result = await getCartController.execute({
+      headers: {
+        authorization: 'Bearer valid-token',
+      },
+    })
+
+    expect(result.status).toBe(200)
+    expect(result.body).toEqual(cartData)
+  })
+})
---
+++ src/adapters/controllers/cart/get-cart-controller.ts
@@ -0,0 +1,36 @@
+import { VerifyTokenUseCase } from '~/domain/auth/application/use-cases/verify-token-use-case'
+import { GetCartUseCase } from '~/domain/cart/application/use-cases/get-cart-use-case'
+
+import { errorHandler } from '~/adapters/controllers/interfaces/error-handler'
+import { Request } from '~/adapters/controllers/interfaces/request'
+
+import { Logger } from '~/infra/logger'
+
+export interface Headers {
+  authorization: string
+}
+
+export class GetCartController {
+  constructor(
+    private logger: Logger,
+    private verifyTokenUseCase: VerifyTokenUseCase,
+    private getCartUseCase: GetCartUseCase
+  ) {}
+
+  async execute({ headers }: Request<undefined, undefined, Headers>) {
+    try {
+      const user = await this.verifyTokenUseCase.execute(headers!.authorization!)
+
+      const result = await this.getCartUseCase.execute(user.id)
+
+      this.logger.info('cart retrieved successfully', { userId: user.id })
+
+      return {
+        status: 200,
+        body: result,
+      }
+    } catch (error) {
+      return errorHandler({ error, logger: this.logger })
+    }
+  }
+}
